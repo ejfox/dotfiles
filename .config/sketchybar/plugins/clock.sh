7f6cf79b03d7d60c0ae3992eb782cd3c10140828
EJ Fox <ejfox@ejfox.com>
2025-11-25 19:30:08 -0500

    fix: remove moon phase from tmux and sketchybar

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-TIME=$(date +"%I:%M %p")
-sketchybar --set $NAME label="$TIME"
+# Source mystical symbols library
+source "$HOME/.dotfiles/lib/mystical-symbols.sh"
+
+# Time with icon
+TIME=$(date +"%I:%M")
+TIME_ICON=$(get_time_of_day_icon)
+
+# Just time icon + time
+sketchybar --set $NAME label="$TIME_ICON $TIME"
